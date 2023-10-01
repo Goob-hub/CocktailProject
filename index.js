7add77bab2fc207e745031a9abd55682376b889e
Goob-hub <solizg02@gmail.com>
2023-10-01 18:23:58 -0500

    Added functionality for requesting a random cocktail recipe and displaying it's basic info.  Also coded dynamic functionality that re-formats the returned data from the cocktail database so it's easier to read and use because it had so many null values and poorly formatted drink ingredients.

--- index.js
+++ index.js
@@ -1,13 +1,87 @@
 import express from "express";
 import axios from "axios";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const url = "https://www.thecocktaildb.com/api/json/v1/1";
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+function cleanObjects(objArr) {
+    objArr.forEach(obj => {
+        //Gets rid of all key's that are equal to null
+        Object.keys(obj).forEach(key => {
+            if(obj[key] == null) {
+                delete obj[key];
+            }
+        });
+    });    
+}
+
+function formatIngredients(obj) {
+    let ingredientsArray = [];
+    let i = 1;
+
+    Object.keys(obj).forEach(key => {
+        //Checks if current key is an ingredient
+        if(key.includes("strIngredient")){
+            let ingredientData = {name: "", amount: ""};
+
+            ingredientData.name = obj[`strIngredient${i}`];
+            ingredientData.amount = obj[`strMeasure${i}`];
+
+            ingredientsArray.push(ingredientData);
+
+            //Deletes old ingredients from drink object
+            delete obj[`strIngredient${i}`];
+            delete obj[`strMeasure${i}`];
+
+            i++;
+        }
+    });
+    return ingredientsArray;
+}
+
+function formatIngredientData(drinksArr){
+    let newDrinksArr = [];
+
+    drinksArr.forEach(drink => {
+        let newIngredientData = formatIngredients(drink);
+        drink["ingredients"] = newIngredientData;
+
+        newDrinksArr.push(drink);
+    });
+
+    return newDrinksArr;
+}
+
+function formatData(drinksArr) {
+    let formattedDrinks;
+
+    cleanObjects(drinksArr);
+    formattedDrinks = formatIngredientData(drinksArr);
+
+    return formattedDrinks;
+}
 
 app.get("/", (req, res) => {
     res.render("index.ejs")
+});
+
+app.post("/random", async (req, res) => {
+    try{
+        //Returns an array of drink objects that are poorly formatted 
+        const response = await axios.get(`${url}/random.php`);
+        
+        //Drink data is now easier to read and use :D!
+        const result = formatData(response.data.drinks);
+        
+        res.render("index.ejs", {drinks: result});
+    } catch(error) {
+        res.render("index.ejs", {recipe: error.message});
+    }
 });
 
 app.listen(PORT, () => {
